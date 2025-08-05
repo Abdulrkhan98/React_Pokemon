514e4f267dd04b96763c83c98adcbf27a3ddf3a4
Abdul R Khan <abdulrkhan98@gmail.com>
2025-08-05 15:32:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Pokemon } from "./Pokemon";
+// import "./Responsive.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Pokemon />
+    </div>
+  );
+};
+
+export default App;
